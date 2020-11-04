fe69f75e0add2374c1be2c9f0b7747c6497aa226
Sam <samtpomarico@gmail.com>
2020-11-04 13:09:50 -0600

    Exercise complete

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -7,18 +7,45 @@ namespace csharp_web_dev_lsn9exceptions
     {
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0)
+            {
+                throw new ArgumentOutOfRangeException("Y is equal to zero.");
+            }
+            double result = x / y;
+            return result;
+
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            int studentPoints;
+            if (fileName == null || fileName == "")
+            {
+                throw new ArgumentNullException("File name is null/non-existent.");
+            }
+            else if (fileName.EndsWith(".cs"))
+            {
+                studentPoints = 1;
+            }
+            else
+            {
+                studentPoints = 0;
+            }
+            return studentPoints;
         }
 
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            try
+            {
+                Divide(3, 0);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -26,8 +53,18 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
-
-
+            
+                foreach(KeyValuePair<string, string> student in students)
+                {
+                    try
+                    {
+                        Console.WriteLine(CheckFileExtension(student.Value));
+                    }
+                    catch(ArgumentNullException e)
+                    {
+                        Console.WriteLine("Filename is null/non-existant.");
+                    }
+                }
         }
     }
 }
